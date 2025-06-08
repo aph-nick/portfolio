f6cd36afa766d4d7a4bace964a00b193b8c7ac4f
Wojtek <hvnterz@gmail.com>
2025-06-08 15:48:17 +0200

    Commit 1

--- app/header.tsx
+++ app/header.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
+import { MapPinIcon } from '@heroicons/react/24/solid'
 
 export function Header() {
   return (
@@ -18,6 +19,14 @@ export function Header() {
         >
           Aspiring Frontend & Backend Developer
         </TextEffect>
+      </div>
+
+      <div className="flex items-center space-x-2 text-zinc-700 dark:text-zinc-400 text-sm font-medium">
+        <MapPinIcon className="h-5 w-5 text-red-600" />
+        <span>Warsaw, Poland</span>
+        <span role="img" aria-label="Polish flag" className="text-xl">
+          🇵🇱
+        </span>
       </div>
     </header>
   )
