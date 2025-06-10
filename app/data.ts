05e060bcb7dc8cc8adea20afcd673fcd88f6af5f
Wojtek <hvnterz@gmail.com>
2025-06-10 14:24:07 +0200

    Commit 1

--- app/data.ts
+++ app/data.ts
@@ -64,7 +64,7 @@ export const PROJECTS: Project[] = [
   {
     name: 'SimpleMail',
     description: 'School Project consisting of a Java application, with Swing components that simulates an email client. Under construction. ',
-    link: 'https://github.com/aph-nick/',
+    link: 'https://github.com/aph-nick/SimpleMail',
     image:
       '/simplemail.jpg',
     id: 'project5',
